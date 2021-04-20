d7e81cbd1801690f7bf5fa4ebb70a6836ac31616
Edu-git1 <eorellan@ucm.es>
2021-04-20 19:44:04 +0200

    Añadidos puntos de guardado

---
+++ Assets/Scripts/Colchon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colchon : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            collision.GetComponent<Respawn>().Llegado(transform.position.x, transform.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private float posicionColchonX, posicionColchonY;
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("posicionColchonX") != 0)
+        {
+            transform.position = new Vector2(PlayerPrefs.GetFloat("posicionColchonX"), PlayerPrefs.GetFloat("posicionColchonY"));
+        }
+    }
+
+    public void Llegado(float x, float y)
+    {
+        PlayerPrefs.SetFloat("posicionColchonX", x);
+        PlayerPrefs.SetFloat("posicionColchonY", y);
+    }
+}
+
