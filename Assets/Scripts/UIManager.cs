36ba7153eb39be9010598d0bd6cb3f590f85bbe3
admont02 <admont02@ucm.es>
2021-04-26 21:56:37 +0200

    Subida la UI provisional

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-   
+    public int heartsRemaining;
+    public Image[] hearts;
     void Start()
     {
         GameManager.GetInstance().SetUIManager(this);
     }
 
-    
+    public void LoseHeart()
+    {
+        heartsRemaining--;
+
+        hearts[heartsRemaining].enabled = false;
+
+        if (heartsRemaining == 0)
+        {
+            Debug.Log("Has perdido");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            LoseHeart();
+        }
+    }
+
+
 }
