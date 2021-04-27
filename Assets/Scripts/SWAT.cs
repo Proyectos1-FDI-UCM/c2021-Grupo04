ac6a1e614672c202455aca981f2580fcf1b36a22
asegar01 <asegar01@ucm.es>
2021-04-27 16:58:53 +0200

    Agregado estados del antidisturbios

--- Assets/Scripts/SWAT.cs
+++ Assets/Scripts/SWAT.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class SWAT : MonoBehaviour
 {
-    float enemyVelocity = 1.5f;
+    [SerializeField] private Transform[] movPoints;
+    private int i = 0;
+    public GameObject player;
+    [SerializeField] private float enemyVelocity = 1.5f;
+
+    private Vector3 iniScale, tempScale, movement;
+    private float right = 1;
+
     Rigidbody2D rb;
-    public bool moveRight;
+    bool charging = false, chargeLeft = false, chargeRight = false;
+    bool moveRight;
+    bool agressive = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        iniScale = transform.localScale;
     }
 
     void Update()
     {
-        if (moveRight)
+        /*if (moveRight)
         {
             transform.Translate(Time.deltaTime * enemyVelocity, 0, 0);
             transform.localScale = new Vector2(1f, 1f);
@@ -24,18 +35,94 @@ public class SWAT : MonoBehaviour
         {
             transform.Translate(-1 * Time.deltaTime * enemyVelocity, 0, 0);
             transform.localScale = new Vector2(-1f, 1f);
-        }
-    }
+        }*/
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (moveRight)
+        if(agressive == false)
         {
-            moveRight = false;
+            transform.position = Vector2.MoveTowards(transform.position, movPoints[i].transform.position, enemyVelocity * Time.deltaTime);
         }
         else
         {
-            moveRight = true;
+            transform.position = transform.position;
         }
+
+        if (Vector2.Distance(transform.position, movPoints[i].transform.position) < 0.5f)
+        {
+            if (movPoints[i] != movPoints[movPoints.Length - 1]) i++;
+            else i = 0;
+            right = Mathf.Sign(movPoints[i].transform.position.x - transform.position.x);
+            Turn(right);
+        }
+    }
+
+    private void Charge(ref bool agressive)
+    {
+        agressive = !agressive;
+        if(agressive == true)
+        {
+            Invoke("Wait", 2);
+        }
+        else
+        {
+            Update();
+        }
+    }
+
+    private void Wait()
+    {
+        Invoke("Charge", 0);
+    }
+
+    private void Turn(float right)
+    {
+        if (right == -1)
+        {
+            tempScale = transform.localScale;
+            tempScale.x = tempScale.x * -1;
+        }
+        else tempScale = iniScale;
+        transform.localScale = tempScale;
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(!collider.gameObject.GetComponent<PlayerController>() && !collider.gameObject.GetComponent<Herropea>())
+        {
+            if (moveRight)
+            {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+        }
+
+        if (!collider.gameObject.GetComponent<Herropea>())
+        {
+            agressive = true;
+            if(rb.transform.position.x < player.transform.position.x)
+            {
+                rb.transform.localScale = new Vector2(-1f, 1f);
+            }
+            if (rb.transform.position.x > player.transform.position.x)
+            {
+                rb.transform.localScale = new Vector2(1f, 1f);
+            }
+        }
+    }*/
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            Charge(ref agressive);
+        }
+        
     }
 }
