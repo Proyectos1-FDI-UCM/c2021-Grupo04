e6a4b85e335f86a46f565d001f39d037f1c0fd78
Miguel González Pérez <miggon23@ucm.es>
2021-05-04 18:36:05 +0200

    Perro arreglado

--- Assets/Scripts/Player/HealthPlayer.cs
+++ Assets/Scripts/Player/HealthPlayer.cs
@@ -7,18 +7,17 @@ public class HealthPlayer : MonoBehaviour
     public int maxHealth = 4;
     public float inmunityCooldown = 1f;
 
+
     private float time = 0;
     private int health;
-    Respawn spawn;
-    
-    
+    Rigidbody2D rb;
 
     private void Start()
     {
        
-        spawn = GetComponent<Respawn>();
         health = maxHealth;
         GameManager.GetInstance().ReDrawHearts(health);
+        rb = GetComponent<Rigidbody2D>();
     } 
 
     public void LoseHearts(int damage)
@@ -29,18 +28,16 @@ public class HealthPlayer : MonoBehaviour
             Debug.Log("Vidas restante del jugador " + health);
             time = Time.time;
             GameManager.GetInstance().HeartDestroyed();
-            
         }
 
         else if (health <= 0)
         {
             GameManager.GetInstance().ExitGame();
             
-            
-
         }
 
     }
+
   
    
 }
