7650a5b6bc20f36ea129c34be6b907b5ca2ae5e5
admont02 <admont02@ucm.es>
2021-05-19 17:08:34 +0200

    implementado el control de volumen y el menu de opciones

---
+++ Assets/Scripts/Game/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingsMenu : MonoBehaviour
+{
+    //Adrián
+    public Slider slider;
+    public float volumeValue;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slider.value;
+    }
+    public void ControlVolume(float volume)
+    {
+        volumeValue = volume;
+        PlayerPrefs.GetFloat("volumenAudio", volumeValue);
+        AudioListener.volume = slider.value;
+    }
+}
+
