7a94f0f37db85b5ceaee66115137001235e14a10
Edu-git1 <eorellan@ucm.es>
2021-05-22 10:54:56 +0200

    Cambio de color al recibir daño

---
+++ Assets/Scripts/Game/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private Renderer rend;
+
+    [SerializeField]
+    private Color cambiocolor = Color.red;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    public IEnumerator Colorear()
+    {
+        rend.material.color = cambiocolor;
+        yield return new WaitForSeconds(0.3f);
+        rend.material.color = Color.white;
+    }
+
+    public void CambiaColor()
+    {
+        StartCoroutine(Colorear());
+    }
+}
