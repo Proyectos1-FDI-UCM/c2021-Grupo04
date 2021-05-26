0be57d4e5627d8a6ddba08ed8e20f7a86c03f88e
miggon23 <miggon23@ucm.es>
2021-05-26 11:53:08 +0200

    Cambios en el decorado

--- Assets/Scripts/Game/SkipCutscene.cs
+++ Assets/Scripts/Game/SkipCutscene.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class SkipCutscene : MonoBehaviour
 {
+
     public float timeToSkip = 12f;
+
+
     void Start()
     {
-        Invoke("SkipScene", timeToSkip);
+        Time.timeScale = 1;
+        Invoke("SkipScene", timeToSkip);       
     }
+
 
     private void SkipScene()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    
 }
